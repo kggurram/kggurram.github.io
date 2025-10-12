9113e7107f49971bf5fee63498b3a694ab971dda
Karthik Gurram <kggurram@gmail.com>
2025-10-11 21:20:53 -0400

    favicon update

--- .next/server/webpack-runtime.js
+++ .next/server/webpack-runtime.js
@@ -130,7 +130,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	(() => {
-/******/ 		__webpack_require__.h = () => ("f4f06957cbddfa06")
+/******/ 		__webpack_require__.h = () => ("6b57ac35162b675f")
 /******/ 	})();
 /******/ 	
 /******/ 	/* webpack/runtime/hasOwnProperty shorthand */
--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -192,7 +192,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "0dea4c1633d38d8a"; }
+/******/ 		__webpack_require__.h = function() { return "0e7a9448a02ac490"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
